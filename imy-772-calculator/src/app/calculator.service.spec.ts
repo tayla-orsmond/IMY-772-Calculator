6400d6d2ed03cc9e2057652377c657c1d368cdd6
Tayla Orsmond <tayla.orsmond@gmail.com>
2024-05-10 19:47:07 +0200

    Subtraction

--- imy-772-calculator/src/app/calculator.service.spec.ts
+++ imy-772-calculator/src/app/calculator.service.spec.ts
@@ -15,7 +15,7 @@ describe('CalculatorService', () => {
   });
 
   // Hex Calculator
-  // limit operations to two numbers only (i.e., no chaining of operations)?
+  // *** limit operations to two numbers only (i.e., no chaining of operations)??
 
   // Addition
   it('should add two 0 hex numbers and return 0', () => {
@@ -48,5 +48,38 @@ describe('CalculatorService', () => {
     let expected = '1FFE';
 
     expect(service.add(n1, n2)).toBe(expected);
+  });
+
+  // Subtraction
+  it('should subtract two 0 hex numbers and return 0', () => {
+    let n1 = '000';
+    let n2 = '000';
+    let expected = '0';
+    
+    expect(service.subtract(n2, n1)).toBe(expected);
+  });
+
+  it('should subtract AAA from BBB and return 111', () => {
+    let n1 = 'AAA';
+    let n2 = 'BBB';
+    let expected = '111';
+
+    expect(service.subtract(n2, n1)).toBe(expected);
+  });
+
+  it('should subtract 56C from 2F3 and return 279 [absolute value]', () => {
+    let n1 = '56C';
+    let n2 = '2F3';
+    let expected = '279';
+
+    expect(service.subtract(n2, n1)).toBe(expected);
+  })
+
+  it('should subtract FFF from FFF and return 0', () => {
+    let n1 = 'FFF';
+    let n2 = 'FFF';
+    let expected = '0';
+
+    expect(service.subtract(n2, n1)).toBe(expected);
   });
 });
