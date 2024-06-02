a2bd884b8d6ca7d0d6ea05f0852501f7d5f8dd54
Tayla Orsmond <tayla.orsmond@gmail.com>
2024-06-02 21:30:30 +0200

    Equation / Result frontend tests

--- imy-772-calculator/cypress/e2e/calculator.cy.ts
+++ imy-772-calculator/cypress/e2e/calculator.cy.ts
@@ -1,11 +1,11 @@
-describe('template spec', () => {
+describe('Calculator e2e', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   // General
   it('should have the correct title', () => {
-    cy.title().should('eq', 'IMY-772-Calculator');
+    cy.title().should('equal', 'IMY-772-Calculator');
   });
 
   it('should have the correct heading', () => {
@@ -13,7 +13,7 @@ describe('template spec', () => {
   });
 
   // Calculator
-  it('should have a calculator, screen and keyboard', () => {
+  it('should have a calculator, screen and keyboard', () => { // check children components exist 
     cy.get('.calculator').should('exist');
     cy.get('.screen').should('exist');
     cy.get('.keyboard').should('exist');
@@ -24,14 +24,16 @@ describe('template spec', () => {
     cy.get('.last-equation-display').should('contain', 'Ans = 0');
   });
 
+  // Display
   it('should display 74B on the screen when 74B is clicked', () => {
     cy.get('[data-cy="7"]').click();
     cy.get('[data-cy="4"]').click();
     cy.get('[data-cy="B"]').click();
-    cy.get('.equation-display').should('contain', '74B');
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', '74B');
   });
 
-  it('should diaplay 821 - 74B on the screen when 821 - 74B is clicked', () => {
+  // Equations
+  it('should display 821 - 74B on the screen when 821 - 74B is clicked', () => {
     cy.get('[data-cy="8"]').click();
     cy.get('[data-cy="2"]').click();
     cy.get('[data-cy="1"]').click();
@@ -39,10 +41,36 @@ describe('template spec', () => {
     cy.get('[data-cy="7"]').click();
     cy.get('[data-cy="4"]').click();
     cy.get('[data-cy="B"]').click();
-    cy.get('.equation-display').should('contain', '821 - 74B');
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', '821 - 74B');
   });
 
-  it('should display D6 on the screen when 821 - 74B is clicked and = is clicked', () => {
+  it('should display 4AA + 98 on the screen when 4AA + 98 is clicked', () => {
+    cy.get('[data-cy="4"]').click();
+    cy.get('[data-cy="A"]').click();
+    cy.get('[data-cy="A"]').click();
+    cy.get('[data-cy="+"]').click();
+    cy.get('[data-cy="9"]').click();
+    cy.get('[data-cy="8"]').click();
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', '4AA + 98');
+  });
+
+  it('should display E1 x 2 on the screen when E1 x 2 is clicked', () => {
+    cy.get('[data-cy="E"]').click();
+    cy.get('[data-cy="1"]').click();
+    cy.get('[data-cy="x"]').click();
+    cy.get('[data-cy="2"]').click();
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', 'E1 x 2');
+  });
+
+  it('should display 8 ÷ C on the screen when 8 ÷ C is clicked', () => {
+    cy.get('[data-cy="8"]').click();
+    cy.get('[data-cy="÷"]').click();
+    cy.get('[data-cy="C"]').click();
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', '8 ÷ C');
+  });
+
+  // Results
+  it('should display D6 on the screen when 821 - 74B = is clicked', () => {
     cy.get('[data-cy="8"]').click();
     cy.get('[data-cy="2"]').click();
     cy.get('[data-cy="1"]').click();
@@ -51,20 +79,38 @@ describe('template spec', () => {
     cy.get('[data-cy="4"]').click();
     cy.get('[data-cy="B"]').click();
     cy.get('[data-cy="="]').click();
-    cy.get('.equation-display').should('contain', 'D6');
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', 'D6');
+    cy.get('[data-cy="lastEquation"]').invoke('text').should('equal', '821 - 74B =');
   });
 
-  it('should clear the screen when clear [AC] is clicked after equation', () => {
-    cy.get('[data-cy="8"]').click();
-    cy.get('[data-cy="2"]').click();
-    cy.get('[data-cy="1"]').click();
-    cy.get('[data-cy="-"]').click();
-    cy.get('[data-cy="7"]').click();
+  it('should display 542 on the screen when 4AA + 98 = is clicked', () => {
     cy.get('[data-cy="4"]').click();
-    cy.get('[data-cy="B"]').click();
+    cy.get('[data-cy="A"]').click();
+    cy.get('[data-cy="A"]').click();
+    cy.get('[data-cy="+"]').click();
+    cy.get('[data-cy="9"]').click();
+    cy.get('[data-cy="8"]').click();
     cy.get('[data-cy="="]').click();
-    cy.get('[data-cy="clear"]').click();
-    cy.get('.equation-display').should('contain', '0');
-    cy.get('.last-equation-display').should('contain', 'Ans = D6');
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', '542');
+    cy.get('[data-cy="lastEquation"]').invoke('text').should('equal', '4AA + 98 =');
+  });
+
+  it('should diaplay 1C2 on the screen when E1 x 2 = is clicked', () => {
+    cy.get('[data-cy="E"]').click();
+    cy.get('[data-cy="1"]').click();
+    cy.get('[data-cy="x"]').click();
+    cy.get('[data-cy="2"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', '1C2');
+    cy.get('[data-cy="lastEquation"]').invoke('text').should('equal', 'E1 x 2 =');
+  });
+
+  it('should display 0 on the screen when 8 ÷ C = is clicked', () => {
+    cy.get('[data-cy="8"]').click();
+    cy.get('[data-cy="÷"]').click();
+    cy.get('[data-cy="C"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="equation"]').invoke('text').should('equal', '0');
+    cy.get('[data-cy="lastEquation"]').invoke('text').should('equal', '8 ÷ C =');
   });
 })
